7ea03f7696ea237efb2b8063d5584a43e6cf38c4
salvatoreolivieri <Salvatore.olivieri07@gmail.com>
2022-06-07 15:47:12 +0200

    Aggiunti esercizi fatti questa mattina

--- Snack-2/index.php
+++ Snack-2/index.php
@@ -1,5 +1,36 @@
 <?php
 
+$valid = true;
+
+$name =  $_GET['name'];
+$mail =  $_GET['mail'];
+$age =  $_GET['age'];
+
+$error = "";
+
+var_dump($_GET);
+
+if (strlen($name) < 3) {
+  $valid = false;
+  $error = "Il tuo nome è inferiore a 3 caratteri.";
+
+} elseif (!strpos($mail, '@') || !strpos($mail, '.')) {
+  $valid = false;
+  $error = "La tua email non contiene il punto o la chiocciola.";
+
+} elseif (!is_numeric($age)){
+  $valid = false;
+  $error = "L'età non corrisponde a un numero";
+
+}
+
+if ($valid) {
+  $permission = "Sei autorizzato";
+  $error = "You rock! Everything oks!";
+
+} else{
+  $permission = "Non sei autorizzato";
+}
 
 
 ?>
@@ -13,6 +44,13 @@
   <title>Snack 2</title>
 </head>
 <body>
-  
+
+<strong>Name: </strong><?php echo $name ?><br>
+<strong>Mail: </strong><?php echo $mail ?><br>
+<strong>Age: </strong><?php echo $age ?><br>
+
+<strong>Output: </strong><?php echo $permission ?><br>
+<strong>Error: </strong><?php echo $error ?><br>
+
 </body>
 </html>
--- Snack-5/index.php
+++ Snack-5/index.php
@@ -1,6 +1,10 @@
 <?php
 
+$paragrafo = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita accusantium libero quasi! Consequatur nisi quaerat accusantium cupiditate consequuntur suscipit obcaecati, exercitationem optio vero velit ab nulla natus quod similique? Ullam consequatur voluptates repellendus ipsum error veniam soluta asperiores magni. Cumque placeat magnam vero doloremque quam, expedita exercitationem quas. Quia, alias. Sint impedit eveniet reiciendis cupiditate maxime in aliquam? Unde nam impedit nostrum doloremque. Labore inventore dolor aspernatur repellendus nisi. Odit earum corrupti ex ut. Dolor cupiditate saepe nulla. Quibusdam unde deserunt voluptates obcaecati aspernatur illo autem voluptas laboriosam, sapiente veritatis at quod soluta, labore doloribus iure. Debitis asperiores natus consectetur?';
 
+$paragrafo_trimmato = explode(".",$paragrafo);
+
+var_dump($paragrafo_trimmato)
 
 ?>
 
@@ -13,6 +17,17 @@
   <title>Snack 5</title>
 </head>
 <body>
-  
+
+<h3>Paragrafo completo</h3>
+<p> <?php echo $paragrafo ?> </p>
+
+<hr>
+
+<h3>Paragrafo trimmato</h3>
+
+<?php for ($i=0; $i < count($paragrafo_trimmato) ; $i++) { 
+  echo "<p> $paragrafo_trimmato[$i] </p><br>" ;
+} ?>
+
 </body>
 </html>
