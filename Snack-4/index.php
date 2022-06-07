6ced9dce3ec25fd39cbc2ce59ee152e4a202110b
salvatoreolivieri <Salvatore.olivieri07@gmail.com>
2022-06-07 17:45:59 +0200

    Snack 4: random number

--- Snack-4/index.php
+++ Snack-4/index.php
@@ -1,6 +1,46 @@
 <?php
 
+//Consegna:
+//Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta
 
+function getUniqueRandom($min, $max, $nItems){
+
+  // creo un array vuoto
+  $newArray = [];
+
+  // verifico che min sia inferiore a max e nel caso lo correggo
+  if($max < $min){
+    $tmp = $max;
+    $max = $min;
+    $min = $tmp;
+  }
+
+  // verifico che non ci siano più items di quelli che si possono estrarre
+  if($nItems > ($max - $min)){
+    $nItems = $max - $min + 1;
+  }
+
+  // creo il ciclo per la creazione dei numeri random
+  // finché non ho raggiunto il numero di items li escraggo e li pusho nell'array da restituire
+  // con count() ottendo la lunghezza dell'array
+  while(count($newArray) < $nItems ){
+    // genero un numero random
+    $number = rand($min, $max);
+    // verifico che non sia presente. In questo caso lo pusho nell'array
+    if(!in_array($number, $newArray)){
+      // in JS sarebbe newArray.push(number)
+      $newArray[] = $number;
+    }
+  }
+
+  // restituisco l'array creato
+  return $newArray;
+
+}
+
+$randArr = getUniqueRandom(1, 100, 15);
+
+var_dump($randArr);
 
 ?>
 
