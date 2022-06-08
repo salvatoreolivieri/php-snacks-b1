5a324c849d8291f671df0ecd76a7c45924ba6595
salvatoreolivieri <Salvatore.olivieri07@gmail.com>
2022-06-08 11:31:11 +0200

    Aggiunto snack 8

--- Snack-7/index.php
+++ Snack-7/index.php
@@ -1,6 +1,54 @@
 <?php
 
+//Consegna:
+//Creare un array contenente qualche alunno di un’ipotetica classe. Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. Stampare Nome, Cognome e la media dei voti di ogni alunno.
 
+$alunni = [
+  [
+    'nome' => 'Davide',
+    'cognome' => 'Pizzola',
+    'voti' => [
+      'matematica' => 9,
+      'italiano' => 7,
+      'fisica' => 9.5,
+      'storia' => 7
+    ]
+  ],
+  [
+    'nome' => 'Ugo',
+    'cognome' => 'de Ughi',
+    'voti' => [
+      'matematica' => 6.25,
+      'italiano' => 8,
+      'fisica' => 5.5,
+      'storia' => 8.5
+    ]
+  ],
+  [
+    'nome' => 'Lello',
+    'cognome' => 'de Lellis',
+    'voti' => [
+      'matematica' => 8.25,
+      'italiano' => 7,
+      'fisica' => 8,
+      'storia' => 7.5
+    ]
+  ],
+];
+
+function mediaVoti($array){
+
+  $somma = 0;
+
+  foreach ($array as $value) {
+    $somma += $value;
+  };
+
+  $media = $somma / count($array);
+
+  return $media;
+
+}
 
 ?>
 
@@ -13,6 +61,19 @@
   <title>Snack 7</title>
 </head>
 <body>
-  
+
+<ul>
+  <?php foreach($alunni as $value): ?>
+
+    <li>
+      <?php echo $value["nome"] ?>
+      <?php echo $value["cognome"] ?>
+      <?php echo "Media: " . mediaVoti($value['voti']) ?>
+    </li>
+
+  <?php endforeach ?>
+</ul>
+
+
 </body>
 </html>
